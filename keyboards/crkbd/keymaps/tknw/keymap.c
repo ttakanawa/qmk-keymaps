89a604ad3d4b6c0a2d053912c9d00fc7b43b0947
Tomonori Takanawa <t@tknw.tw>
2026-03-18 02:08:42 +0800

    feat: reorganize Layer 1 symbol key positions
    Swap backtick/tilde placement on left side.
    Replace percent with NA. Move backslash and pipe
    positions on right side for better workflow.

--- keyboards/crkbd/keymaps/tknw/keymap.c
+++ keyboards/crkbd/keymaps/tknw/keymap.c
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // inspired by https://shop.keyboard.io/pages/the-atreus-layout
   [1] = LAYOUT_split_3x6_3_ex2(
   //,--------------------------------------------------------------.  ,--------------------------------------------------------------.
-           NA, KC_EXLM,   KC_AT, KC_HASH, KC_AMPR, KC_ASTR,      NA,    QK_BOOT, KC_PERC,    KC_7,    KC_8,    KC_9, KC_MINS,      NA,
+           NA, KC_EXLM,   KC_AT, KC_HASH,  KC_GRV, KC_TILD,      NA,    QK_BOOT,      NA,    KC_7,    KC_8,    KC_9, KC_MINS,      NA,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-           NA, KC_LPRN, KC_CIRC, KC_BSLS,  KC_DLR, KC_RPRN, _______,         NA,  KC_GRV,    KC_4,    KC_5,    KC_6,  KC_EQL,      NA,
+           NA, KC_LPRN, KC_CIRC, KC_PIPE,  KC_DLR, KC_RPRN, _______,         NA, KC_BSLS,    KC_4,    KC_5,    KC_6,  KC_EQL,      NA,
   //|--------+--------+--------+--------+--------+--------+--------'  `--------+--------+--------+--------+--------+--------+--------|
            NA, KC_LBRC, KC_RBRC, KC_QUOT, KC_LCBR, KC_RCBR,                      KC_BSPC,    KC_1,    KC_2,    KC_3, ZERO_SFT,     NA,
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
